771026900c50a16233e3fba957c9ea0e45bca7cd
Dominik Steinmann <Dominik.steinmann@gmail.com>
2015-10-08 21:36:33 +0200

    Create brew-cask.sh

---
+++ brew-cask.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# to maintain cask .... 
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup` 
+
+
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install spectacle
+brew cask install dropbox
+brew cask install gyazo
+brew cask install rescuetime
+
+
+# dev
+brew cask install antetype
+brew cask install colorpicker-antetype
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install imagealpha
+brew cask install imageoptim
+
+# fun
+brew cask install limechat
+brew cask install miro-video-converter
+brew cask install horndis               # usb tethering
+
+# browsers
+brew cask install google-chrome-canary
+brew cask install google-chrome
+brew cask install firefox-nightly
+brew cask install webkit-nightly
+
+# less often
+brew cask install disk-inventory-x
+brew cask install screenflow4 # 4 specifically not 5.
+brew cask install vlc
+brew cask install gpgtools
+brew cask install licecap
+

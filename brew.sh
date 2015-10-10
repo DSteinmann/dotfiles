9886aefafbcdd7c1d2034ba36675e9857ccc0d63
Dominik Steinmann <dominik@steinmann.io>
2015-10-10 22:55:30 +0200

    added check for xcode cl tools

--- brew.sh
+++ brew.sh
@@ -5,6 +5,26 @@
 # Ask for the administrator password upfront.
 sudo -v
 
+##############################################################################################################
+### XCode Command Line Tools
+#      thx  https://github.com/alrra/dotfiles/blob/c2da74cc333/os/os_x/install_applications.sh#L39
+
+if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
+  xcode-select --install &> /dev/null
+  # Wait until the XCode Command Line Tools are installed
+  while [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; do
+    sleep 5
+  done
+  xcode-select -p &> /dev/null
+  if [ $? -eq 0 ]; then
+    # Prompt user to agree to the terms of the Xcode license
+    # https://github.com/alrra/dotfiles/issues/10
+    sudo xcodebuild -license
+  fi
+fi
+###
+##############################################################################################################
+
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
